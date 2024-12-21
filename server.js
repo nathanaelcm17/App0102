99de49a5fe2eab50804b88c0d0aa26cf58f44e51
nathanael <nathanael.corporan@hotmail.com>
2024-12-21 13:10:38 -0400

    Cambios en server.js para corregir bug

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-import { createServer } from "http";
-import fs from "fs"; // Para leer archivos
-import path from "path"; // Para manejar rutas de archivos
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const server = createServer((req, res) => {
-  // Definir la ruta del archivo HTML
+const server = http.createServer((req, res) => {
   const filePath = path.join(__dirname, "index.html");
 
-  // Verificar si el archivo existe
   fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
       res.writeHead(500, { "Content-Type": "text/plain" });
@@ -14,12 +12,11 @@ const server = createServer((req, res) => {
       return;
     }
 
-    // Si el archivo se encuentra, servirlo con código 200
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
   });
 });
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log("Servidor ejecutándose en http://localhost:3001");
 });
